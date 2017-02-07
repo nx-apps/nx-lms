ad6e27ab80afee38253768a2d0c113e058fb7bc2
chonlatee <chonlatee.s@nexts-corp.com>
2017-02-07 16:47:40 +0700

    update

--- app/controllers/question/index.controller.js
+++ app/controllers/question/index.controller.js
@@ -14,9 +14,23 @@ class index{
         })
     }
 
-    insert_question(req,res){
+    select_question_only(req,res){
         var r = req._r;
         var params = req.query;
+
+        r.db('lms').table('question').get(params.id)
+        .run()
+        .then(function(result){
+            res.json(result);
+        })
+        .catch(function(err){
+            res.status(500).json(err);
+        })
+    }
+
+    insert_question(req,res){
+        var r = req._r;
+        var params = req.body;
 
         r.db('lms').table('question').insert(params)
         .run()
--- app/routes/question.routes.js
+++ app/routes/question.routes.js
@@ -1,5 +1,6 @@
 module.exports = function(app){
     var index = require('../controllers/question/index.controller');
     app.get('/question',index.select_question);
+    app.get('/question_only',index.select_question_only);
     app.post('/question',index.insert_question);
 }
