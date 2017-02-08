495ac56736502d279f4d7928788337742dd1331c
Kitz-Parnupat <dainokung2535@gmail.com>
2017-02-08 18:43:23 +0700

    update page classroom

--- public/src/redux-store/reducer/classRoom.js
+++ public/src/redux-store/reducer/classRoom.js
@@ -7,7 +7,7 @@ const initialState = {
 export function classRoomReducer(state = initialState,action){
 
     switch (action.type) {
-        case 'CLASS_GET_LIST':
+        case 'CLASSROOM_GET_LIST':
             return Object.assign({},state,{dataList:action.payload});
         default:
             return state
@@ -17,6 +17,15 @@ export function classRoomReducer(state = initialState,action){
 
 export function classRoomAction(store){
     return {
-        
+        CLASSROOM_GET_LIST:function(){
+            axios.get('./student/student')
+            .then((response)=>{
+                console.log(response.data);
+                store.dispatch({type:'CLASSROOM_GET_LIST',payload:response.data});
+            })
+            .catch((error)=>{
+                console.log(error);
+            })
+        },
     }
 }
