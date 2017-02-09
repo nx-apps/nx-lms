d6a1ec7dbd9c9f84961171813833c33cf07afaad
chonlatee <chonlatee.s@nexts-corp.com>
2017-02-09 12:21:16 +0700

    update

--- app/controllers/examination/index.controller.js
+++ app/controllers/examination/index.controller.js
@@ -60,7 +60,7 @@ class index{
             description:          params.description,
             ex_name:              params.ex_name,
             obj:                  params.obj,
-            obj_tag:              params.obj_tag,
+            //obj_tag:              params.obj_tag,
             qty:                  params.qty,
             question:             params.question,
             time:                 params.time,
