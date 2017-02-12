bfbd5f082a7298bd01911fd63cbe2f162c2bd553
chonlatee <chonlatee.s@nexts-corp.com>
2017-02-12 22:02:43 +0700

    update

--- app/controllers/upload_exam/index.controller.js
+++ app/controllers/upload_exam/index.controller.js
@@ -6,25 +6,35 @@ class index{
         var  XLSX = require('xlsx');
         var workbook = XLSX.readFile('D:/rethinkdb-2.3.5/excel/Workbook1.xlsx');
 
-        var ch = [], allData=[], count=0;
+        var allData=[], count_name=0, count_round=0;
         var sheet_name_list = workbook.SheetNames;
         
         sheet_name_list.forEach(function(y) { 
         var worksheet = workbook.Sheets[y];
-        var i = 0;
-            for (var z in worksheet){
+        var i=0, ch=[];
+        
+            for (var z in worksheet){  //sheets round
                 if(z[0] === '!') continue;
                 if((worksheet[z].v === 'q') || (worksheet[z].v === 'ch1') || (worksheet[z].v === 'ch2') || (worksheet[z].v === 'ch3')|| 
                 (worksheet[z].v === 'ch4') || (worksheet[z].v === 'ch5')) continue;
-                ch[i] = worksheet[z].v; i++;
-            }
-            ch[i] = sheet_name_list[count];
-            allData[count] = ch; count++; ch = [];
-        });
 
+                if(i <= 5){
+                    ch[i] = worksheet[z].v; 
+                     if(i==5){
+                         ch[i+1] = sheet_name_list[count_name];
+                         allData[count_round] = ch;
+                         ch=[]; i=-1; count_round++;
+                     }
+                     i++;
+                }
+
+            }
+            count_name++;
+        });
+        //console.log(allData);
         var getLength = allData[0].length-1; // check exam
-        var all = [];
-        var data = {topic:"",choice:[],answer:0,tag:[]};
+        var all=[], data = {topic:"",choice:[],answer:0,tag:[]};
+
         allData.map((row,i)=>{
         row.map(function(row2,i2){
             if(i2==0){
@@ -71,6 +81,7 @@ class index{
         .catch(function(err){
             res.status(500).json(err);
         })   
+    
     }
 }
 
