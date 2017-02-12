f4244ef120b3f957f0c2ea06f8f48a42f4fff52a
พงศ์ปณต ทัศนียาชุมพาลี <namkhaeng@Macintosh.local>
2017-02-12 16:56:01 +0700

    async

--- app/controllers/push/push.controller.js
+++ app/controllers/push/push.controller.js
@@ -1,50 +1,56 @@
 const fs = require('fs')
-
+const async = require('async')
 const importList = require('./import');
-const importHTML = fs.readFileSync('./public/src/import2.html');
 
-const pushy = (req,res,list)=>{
-
-    list.map((row)=>{
-        var contentType;
-        switch (row.type) {
-            case "html":
-                contentType = "text/html";
-                break;
-            case "js":
-                contentType = "application/javascript";
+var pusher = function (res, callback) {
+    async.each(importList, function (push_file, next) {
+        try {
+            var contentType;
+            switch (push_file.type) {
+                case "html":
+                    contentType = "text/html";
+                    break;
+                case "js":
+                    contentType = "application/javascript";
+            }
+            fs.readFile('./public/' + push_file.path, function (err, content) {
+                res.push(push_file.path,
+                    {
+                        status: 200,
+                        method: 'GET',
+                        request: {
+                            accept: '*/*'
+                        },
+                        response: {
+                            'content-type': contentType
+                        }
+                    }
+                    ,
+                    function (err, stream) {
+                        if (err) next(err);
+                        stream.end(content);
+                        next();
+                    }
+                );
+            });
+        } catch (err) {
+            next(err);
         }
-        
-        const importFile = fs.readFileSync('./public/'+row.path);
-
-        res.push(row.path, 
-            {
-                status: 200,
-                method: 'GET',
-                request: {
-                    accept: '*/*'
-                },
-                response: {
-                    'content-type':contentType
-                }
-            }
-            ,
-            function(err, stream){
-                console.log('*************************',err);
-                if (err) return;
-                stream.end(importFile);
-            }
-        )
-    })
-
+    }, function (err) {
+        callback(err);
+    });
 }
 
-class pusha{
-    import(req,res){
-        
-        pushy(req,res,importList);
-        res.end(importHTML)
-        //res.json(importList);
+class pusha {
+    import(req, res) {
+        pusher(res, function (err) {
+            res.end("");
+        });
     }
 }
 module.exports = new pusha();
+
+
+
+
+
