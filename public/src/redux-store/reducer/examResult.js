d484f6b0da3b89ea32c608347d9ac516d28ca89b
พงศ์ปณต ทัศนียาชุมพาลี <namkhaeng@Macintosh.local>
2017-02-14 18:07:06 +0700

    sync

--- public/src/redux-store/reducer/examResult.js
+++ public/src/redux-store/reducer/examResult.js
@@ -18,14 +18,11 @@ export function examResultReducer(state = initialState,action){
 export function examResultAction(store){
     return [commonAction(),
         {
-            EXAM_RESULT_GET_RESULT:function(exam_room_id){
-                var user_id = store.getState().auth.user.id;
-                console.log(exam_room_id,'...',user_id);
+            EXAM_RESULT_GET_RESULT:function(exam_room_id,user_id=store.getState().auth.user.id){
+                console.log(exam_room_id);
+                //console.log(exam_room_id,'...',user_id);
                 axios.get('./send_answer/show_answer',{
-                params:{
-                    exam_room_id: exam_room_id,
-                    user_id:user_id
-                }
+                    params:{exam_room_id,user_id}
                 })
                 .then((response)=>{
                     console.log('success!!');
