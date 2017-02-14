6f4ca94a66951d30c52c6dfb0f1e7550e40e7204
พงศ์ปณต ทัศนียาชุมพาลี <namkhaeng@Macintosh.local>
2017-02-14 13:19:22 +0700

    fixbug upload question

--- app/controllers/question/index.controller.js
+++ app/controllers/question/index.controller.js
@@ -102,123 +102,43 @@ class index{
             var  XLSX = require('xlsx');
             var workbook = XLSX.readFile(xx);
 
-            var allData=[], count_name=0, count_round=0;
-            var sheet_name_list = workbook.SheetNames;
-            
-            sheet_name_list.forEach(function(y) { 
-            var worksheet = workbook.Sheets[y];
-            var i=0, ch=[];
-            
-                for (var z in worksheet){  //sheets round
-                    if(z[0] === '!') continue;
-                    if((worksheet[z].v === 'q') || (worksheet[z].v === 'ch1') || (worksheet[z].v === 'ch2') || (worksheet[z].v === 'ch3')|| 
-                    (worksheet[z].v === 'ch4') || (worksheet[z].v === 'ch5')) continue;
-
-                    if(i <= 5){
-                        ch[i] = worksheet[z].v; 
-                        if(i==5){
-                            ch[i+1] = sheet_name_list[count_name];
-                            allData[count_round] = ch;
-                            ch=[]; i=-1; count_round++;
-                        }
-                        i++;
-                    }
-
-                }
-                count_name++;
-            });
-            //console.log(allData);
-            var getLength = allData[0].length-1; // check exam
-            var all=[], data = {topic:"",choice:[],answer:0,tag:[]};
-
-            allData.map((row,i)=>{
-            row.map(function(row2,i2){
-                if(i2==0){
-                    data.topic = row2;
-                }else if(i2 != getLength){
-                    if(i2==1){
-                        data.choice.push(
-                            {
-                                checked: true,
-                                chice:row2,
-                                image_id:""
-                            }
-                        )
-                    }else{
-                        data.choice.push(
-                            {
-                                checked: false,
-                                chice:row2,
-                                image_id:""
-                            }
-                        )
-                    }
-                }
-                else{
-                    data.tag.push(row2);
-                }
-            })
-                all.push(data);
-                data = {topic:"",choice:[],answer:0,tag:[]};
-            })
-/*
-            var ch = [], allData=[], count=0;
-            var sheet_name_list = workbook.SheetNames;
-            
-            sheet_name_list.forEach(function(y) { 
-            var worksheet = workbook.Sheets[y];
-            var i = 0;
-                for (var z in worksheet){
-                    if(z[0] === '!') continue;
-                    if((worksheet[z].v === 'q') || (worksheet[z].v === 'ch1') || (worksheet[z].v === 'ch2') || 
-                    (worksheet[z].v === 'ch3') || (worksheet[z].v === 'ch4') || (worksheet[z].v === 'ch5')) continue;
+            var data = workbook.Sheets;
+            let preData = [];
+            let mainIndex = -1;
+            for (var sheet in data) {
+                for (var key in data[sheet]) {
                     
-                    ch[i] = worksheet[z].v; i++;
-                }
-                ch[i] = sheet_name_list[count];
-                allData[count] = ch; count++; ch = [];
-            });
+                    if (key !== '!ref') {
+                        if (key[0] === 'A') {
+                            mainIndex++;
+                            preData[mainIndex] = {
+                                topic:data[sheet][key].v,
+                                tag:[sheet],
+                                answer:0,
+                                choice:[]
 
-            var getLength = allData[0].length-1; // check exam
-            var all = [];
-            var data = {topic:"",choice:[],answer:0,tag:[]};
-            allData.map((row,i)=>{
-            row.map(function(row2,i2){
-                if(i2==0){
-                    data.topic = row2;
-                }else if(i2 != getLength){
-                    if(i2==1){
-                        data.choice.push(
-                            {
-                                checked: true,
-                                chice:row2,
-                                image_id:""
-                            }
-                        )
-                    }else{
-                        data.choice.push(
-                            {
-                                checked: false,
-                                chice:row2,
-                                image_id:""
-                            }
-                        )
+                            };
+                            var index = 0;
+                        }else{
+                            var choice = {checked: false,image_id:""};
+                            if(index===0) choice.checked = true;
+                            index++;
+                            choice.choice = data[sheet][key].v;
+                            preData[mainIndex].choice.push(choice);
+                        }
                     }
                 }
-                else{
-                    data.tag.push(row2);
-                }
+            }
+
+            preData = preData.filter(function(row){
+                return (row.topic==='q')?false:true
             })
-                all.push(data);
-                data = {topic:"",choice:[],answer:0,tag:[]};
-            })
-*/
-            r.expr(all).merge(function(x){
+
+            r.expr(preData).merge(function(x){
                 return { time_insert:r.now(),user_id:fields.user_id[0] }
             }).do(function(result){
                 return r.db('lms').table('question').insert(result)
             })
-
             .run()  
             .then(function(result){
                 res.json(result);
@@ -226,6 +146,7 @@ class index{
             .catch(function(err){
                 res.status(500).json(err);
             })
+
 
         });
         
