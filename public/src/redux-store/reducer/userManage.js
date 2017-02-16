421df385b242bb4e43f7878c3c3fe9e55446a475
Kitz-Parnupat <dainokung2535@gmail.com>
2017-02-16 14:59:17 +0700

    store userManage

---
+++ public/src/redux-store/reducer/userManage.js
@@ -0,0 +1,43 @@
+import axios from '../axios'
+import {commonAction} from '../config'
+
+const initialState = {
+    dataList:[]
+}
+
+export function userManageReducer(state = initialState,action){
+    switch (action.type) {
+        case 'USER_MANAGE_GET_LIST':
+            return Object.assign({},state,{dataList:action.payload});
+        case 'USER_MANAGE_CLEAR_LIST':
+            return Object.assign({},state,{dataList:action.payload});
+        default:
+            break;
+    }
+}
+
+export function userManageAction(store){
+    return [commonAction(),
+        {
+            USER_MANAGE_GET_LIST:function(){
+                this.fire('toast',{status:'load'});
+                axios.get('./user/user/')
+                .then((response)=>{
+                    console.log(response.data);
+                    this.fire('toast',{status:'success',
+                      callback:function(){
+                      }
+                     });
+                    store.dispatch({type:'USER_MANAGE_GET_LIST',payload:response.data});
+                })
+                .catch((error)=>{
+                    console.log('error');
+                    console.log(error);
+                });
+            },
+            USER_MANAGE_CLEAR_LISTL:function(){
+                 store.dispatch({type:'USER_MANAGE_GET_LIST',payload:{}});
+            }        
+        }
+    ]
+}
