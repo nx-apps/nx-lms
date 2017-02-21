86bb8649b39820807092243168cadc86ea4a0940
Kitz-Parnupat <dainokung2535@gmail.com>
2017-02-21 17:08:19 +0700

    update

--- public/src/redux-store/reducer/exam.js
+++ public/src/redux-store/reducer/exam.js
@@ -18,7 +18,9 @@ export function examAction(store){
     return [
         commonAction(),{
             EXAM_INSERT_DATA:function(data){
+                console.log(JSON.stringify(data));
                 data.user_id = store.getState().auth.user.id;
+              
                 this.fire('toast',{status:'load'});
                 axios.post('./send_answer/send_answer',data)
                 .then((response)=>{
