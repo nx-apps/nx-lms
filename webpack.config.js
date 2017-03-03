85f82fcc1a89f9b8effd78375cd2eb099c0fcc68
Pongpanot Tassaneeyachumphalee <pongpanot.t@nexts-corp.com>
2017-03-03 11:50:24 +0700

    update to es5

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,17 @@ module.exports = {
     output: {
         filename: './public/bundle.js'
     },
+    module:{
+        loaders:[
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loaders: [
+                'babel-loader'
+                ]
+            }
+        ]
+    },
     watch: true,
     devtool:"inline-source-map"
 };
