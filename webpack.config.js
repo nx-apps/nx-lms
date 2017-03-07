33faaf9b52a781da61a7a4b1732f40e95e18292a
Pongpanot Tassaneeyachumphalee <pongpanot.t@nexts-corp.com>
2017-03-07 23:26:35 +0700

    init

--- webpack.config.js
+++ webpack.config.js
@@ -5,17 +5,28 @@ module.exports = {
     output: {
         filename: './public/bundle.js'
     },
-    module:{
-        /*loaders:[
+    module: {
+        loaders: [
+            {
+                test: /\.html$/,
+                loader: 'babel-loader!wc-loader',
+            },
             {
                 test: /\.js?$/,
                 exclude: /node_modules/,
                 loaders: [
-                'babel-loader'
+                    'babel-loader'
                 ]
-            }
-        ]*/
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader',
+                query: {
+                    name: '[name].[ext]?[hash]'
+                }
+            },
+        ]
     },
     watch: true,
-    devtool:"inline-source-map"
+    devtool: "inline-source-map"
 };
